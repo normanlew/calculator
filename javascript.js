c8d51a252af66b7e499524525fc3ea6dbf8e0986
Norman Lew <normanlew@users.noreply.github.com>
2026-02-21 14:09:03 -0800

    Create functions to perform basic arithmetic
    Wrote add, subtract, multiply and divide functions and tested them.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-let state = "clear" // possible values: clear, firstOperand, secondOperand, 
+// let state = "clear" // possible values: clear, firstOperand, secondOperand, 
 let firstOperand = "";
 let secondOperand = "";
 let operator = "";
@@ -10,14 +10,67 @@ function clear() {
     secondOperand = "";
     operator = "";
     displayText.value = "0";
-    state = "clear";
+    // state = "clear";
 }
 
 clear();
 
 function handleButtonPush(x) {
     console.log("You pushed " + x);
-    clear();
+    // let xNumeric = Number(x);
+
+    // // console.log(xNumeric);
+    // if (Number.isNaN(xNumeric)) {
+    //     console.log(x + " is not a number");
+    // }
+    // else {
+    //     console.log(x + " is a number");
+    // }
+
+    // if (x === "\u00F7") {
+    //     console.log("true");
+    // }
+
+
+    // console.log(typeof(x));
+    // let isNumber = typeof(Number(x));
+    // console.log(Number(x));
+    // console.log(isNumber);
+
+    // if (Number.isNaN(isNumber)) {
+    //     console.log(x + " is not a number");
+    // }
+    // else {
+    //     console.log(x + " is a number");
+    // }
+
+    // if (isNumber === "number") {
+    //     console.log(x + " is a number");
+    // }
+    // else {
+    //     console.log(x + " is not a number");
+    // }
+
+    // clear();
+    // switch(state) {
+
+    // }
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
 }
 
 let buttons = document.querySelectorAll("button");
@@ -29,3 +82,10 @@ for (const button of buttons) {
         handleButtonPush(button.textContent);
     });
 }
+
+
+
+
+// \u2212 = minus
+
+// \u00F7 = divide
