8f9f30f1a0c13e6ec0af10ae8951da26452b121a
Norman Lew <normanlew@users.noreply.github.com>
2026-02-25 16:55:51 -0800

    Refactor and include state
    Refactored code to include a state based on where the calculator is in terms of
    numbers having been entered and whether a calculation has been performed
    already.  Refactored up to + / - sign functionality.

--- javascript.js
+++ javascript.js
@@ -1,16 +1,21 @@
-let firstOperand = true;
+// let firstOperand = true;  // possible values of state are firstOperand, secondOperand
+// let postOperation = false;
 // let firstNumber = true;
-let firstOperandValue = "";
+let state = "firstOperand"; // possible values of state are firstOperand, secondOperand and postCalculation
+let firstOperandValue = "0";
+let secondOperandValue = "0";
 // let secondOperand = "0";
-let operator = "";
+let operatorValue = "";
+// let numberShowing = "";
 
 const displayText = document.getElementById("result");
 
 function clear() {
-    firstOperand = true;
-    firstOperandValue = "";
+    state = "firstOperand";
+    firstOperandValue = "0";
+    secondOperandValue = "0"
     // secondOperand = "0";
-    operator = ""
+    operatorValue = ""
     displayText.value = "0";
     // displayText.value = "firstOperand";
     // state = "clear";
@@ -19,7 +24,8 @@ function clear() {
 clear();
 
 function handleButtonPush(buttonValue) {
-    // console.log("You pushed " + buttonValue);
+    // console.log(buttonValue === "-");
+    console.log("You pushed " + buttonValue);
     // console.log(buttonValue === "+ / −")
     // console.log(buttonValue === "9")
     // firstOperand += x;
@@ -32,48 +38,255 @@ function handleButtonPush(buttonValue) {
         // let displayedValue = displayText.value;
         let buttonValueNumeric = Number(buttonValue);
         let buttonValueNumericIsNan = Number.isNaN(buttonValueNumeric);
-        let numberShowing = displayText.value;
+        // let numberToShow = "";
+        
+        if (!buttonValueNumericIsNan) {     
+            switch(state) {
+                case "firstOperand": 
+                    if (buttonValue === "0") {
+                        if (!(firstOperandValue === "0" || firstOperandValue === "-0")) {
+                            firstOperandValue += buttonValue;
+                        }
+                    }
+                    else {
+                        if (firstOperandValue === "0") {
+                            firstOperandValue = buttonValue;
+                        }
+                        else if (firstOperandValue === "-0") {
+                            firstOperandValue = "-" + buttonValue;
+                        }
+                        else firstOperandValue += buttonValue;
+                    }
+                    displayText.value = firstOperandValue;
+                    break;
 
-        if (!buttonValueNumericIsNan)  {
-            if (buttonValue === "0") {
-                if (!(numberShowing === "0" || numberShowing === "-0")) {
-                    numberShowing += buttonValue;
-                }
+                case "secondOperand":
+                    if (buttonValue === "0") {
+                        if (!(secondOperandValue === "0" || secondOperandValue === "-0")) {
+                            secondOperandValue += buttonValue;
+                        }
+                    }
+                    else {
+                        if (secondOperandValue === "0") {
+                            secondOperandValue = buttonValue;
+                        }
+                        else if (secondOperandValue === "-0") {
+                            secondOperandValue = "-" + buttonValue;
+                        }
+                        else secondOperandValue += buttonValue;
+                    }
+                    displayText.value = secondOperandValue;
+                    break;
+
+                case "postCalculation":
+                    firstOperandValue = buttonValue;
+                    state = firstOperandValue;
+                    displayText.text = firstOperandValue;
+
             }
-            else {
-                if (numberShowing === "0"){
-                    numberShowing = buttonValue;
-                }
-                else if (numberShowing === "-0") {
-                    numberShowing = "-" + buttonValue;
-                }
-                else {
-                    numberShowing += buttonValue;
-                }
-            }
-            displayText.value = numberShowing;
+                    
         }    
+            // if (postOperation) {
+            //     numberToShow = buttonValue;
+            //     // postOperation = false;
+            // }
+            // else {
+            // // postOperation = false;
+            //     if (buttonValue === "0") {
+            //         if (!(numberShowing === "0" || numberShowing === "-0")) {
+            //             numberShowing += buttonValue;
+            //         }
+            //     }
+            //     else {
+            //         if (numberShowing === "0") {
+            //             numberShowing = buttonValue;
+            //         }
+            //         else if (numberShowing === "-0") {
+            //             numberShowing = "-" + buttonValue;
+            //         }
+            //         else numberShowing += buttonValue;
+            //     }
+            // }
+            // // }
+            // displayText.value = numberShowing;
+            // if (firstOperand) {
+            //     firstOperandValue = numberShowing;
+            // }
+            // else {
+            //     secondOperandValue = numberShowing;
+            // }
+            
+            // // else {
+            // //     if (buttonValue === "0") {
+            // //         if (!(secondOperandValue === "0" || secondOperandValue === "-0")) {
+            // //             secondOperandValue += buttonValue;
+            // //         }
+            // //     }
+            // //     else {
+            // //         if (secondOperandValue === "0") {
+            // //             secondOperandValue = buttonValue;
+            // //         }
+            // //         else if (secondOperandValue === "-0") {
+            // //             secondOperandValue = "-" + buttonValue;
+            // //         }
+            // //         else secondOperandValue += buttonValue;
+            // //     }
+            // //     displayText.value = secondOperandValue;
+            // // }
+        
+
+            // if (state === "operator") {
+            //     numberShowing = "0";
+            // }
+            // else {
+            //     numberShowing = displayText.value;
+            // }
+            // if (buttonValue === "0") {
+            //     if (!(numberShowing === "0" || numberShowing === "-0")) {
+            //         numberShowing += buttonValue;
+            //     }
+            // }
+            // else {
+            //     if (numberShowing === "0"){
+            //         numberShowing = buttonValue;
+            //     }
+            //     else if (numberShowing === "-0") {
+            //         numberShowing = "-" + buttonValue;
+            //     }
+            //     else {
+            //         numberShowing += buttonValue;
+            //     }
+            // }
+            // displayText.value = numberShowing;
+          
         else if (buttonValue === "+ / −") {
-            console.log("buttonValue is + -");
-            if (numberShowing === "0") {
-                numberShowing = "-0";
+            // postOperation = false;
+            console.log("buttonValue is: " + buttonValue);
+
+            switch (state) {
+                case "firstOperand":
+                    if (firstOperandValue === "0") {
+                        firstOperandValue = "-0";
+                    }
+                    else if (firstOperandValue === "-0") {
+                        // console.log("number showing is " + numberShowing);
+                        firstOperandValue = "0";
+                    }
+                    else {
+                        let firstOperandValueNumeric = -Number(firstOperandValue);
+                        firstOperandValue = firstOperandValueNumeric.toString();
+                    }
+                    displayText.value = firstOperandValue;
+                    break;
+
+                case "postCalculation":
+                    if (firstOperandValue === "0") {
+                        firstOperandValue = "-0";
+                    }
+                    else if (firstOperandValue === "-0") {
+                        // console.log("number showing is " + numberShowing);
+                        firstOperandValue = "0";
+                    }
+                    else {
+                        let firstOperandValueNumeric = -Number(firstOperandValue);
+                        firstOperandValue = firstOperandValueNumeric.toString();
+                    }
+                    displayText.value = firstOperandValue;
+                    // state = "firstOperand"
+                    break;
+
+                case "secondOperand":
+                    if (secondOperandValue === "0") {
+                        secondOperandValue = "-0";
+                    }
+                    else if (secondOperandValue === "-0") {
+                        // console.log("number showing is " + numberShowing);
+                        secondOperandValue = "0";
+                    }
+                    else {
+                        let secondOperandValueNumeric = -Number(secondOperandValue);
+                        secondOperandValue = secondOperandValueNumeric.toString();
+                    }
+                    displayText.value = secondOperandValue;
+                    break;
             }
-            else if (numberShowing === "-0") {
-                // console.log("number showing is " + numberShowing);
-                numberShowing = "0";
-            }
-            else {
-                let numberShowingNumeric = -Number(numberShowing);
-                numberShowing = numberShowingNumeric.toString();
-            }
-            displayText.value = numberShowing;
+ 
+            // let numberShowing = displayText.value;
+            // if (numberShowing === "0") {
+            //     numberShowing = "-0";
+            // }
+            // else if (numberShowing === "-0") {
+            //     // console.log("number showing is " + numberShowing);
+            //     numberShowing = "0";
+            // }
+            // else {
+            //     let numberShowingNumeric = -Number(numberShowing);
+            //     numberShowing = numberShowingNumeric.toString();
+            // }
+            // displayText.value = numberShowing;
+            // if (firstOperand) {
+            //     firstOperandValue = numberShowing;
+            // }
+            // else {
+            //     secondOperandValue = numberShowing;
+            // }
+            // if (numberShowing === "0") {
+            //     numberShowing = "-0";
+            // }
+            // else if (numberShowing === "-0") {
+            //     // console.log("number showing is " + numberShowing);
+            //     numberShowing = "0";
+            // }
+            // else {
+            //     let numberShowingNumeric = -Number(numberShowing);
+            //     numberShowing = numberShowingNumeric.toString();
+            // }
+            // displayText.value = numberShowing;
         }
 
         else if (buttonValue === ".") {
+            postOperation = false;
             if (!numberShowing.includes(".")) {
                 numberShowing += ".";
                 displayText.value = numberShowing;
             }
+            if (firstOperand) {
+                firstOperandValue = numberShowing;
+            }
+            else {
+                secondOperandValue = numberShowing;
+            }
+        }
+        else if (["÷", "−", "+", "X"].includes(buttonValue)) {
+            postOperation = false;
+            if (firstOperand) {
+                firstOperand = false;
+                // firstOperatorValue = numberShowing;
+            }
+            operator = buttonValue;
+        }
+        else if (buttonValue === "=") {
+            postOperation = false;
+            switch (operator) {
+                case "+":
+                    firstOperandValue = add(firstOperand, secondOperand);
+                    break;
+
+                case "-":
+                    firstOperandValue = subtract(firstOperand, secondOperand);
+                    break;
+
+                case "X":
+                    firstOperandValue = multiply(firstOperand, secondOperand);
+                    break;
+
+                case "÷":
+                    firstOperandValue = divide(firstOperand, secondOperand);
+                    break;
+            }
+            displayText.value = firstOperandValue;
+            firstOperand = true;
+            postOperation = true;
         }
     }
 
